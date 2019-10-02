0cf3da072ca06587be85ac4e4b0ad43ed9563bed
Glimglob <54424684+Glimglob@users.noreply.github.com>
2019-10-02 13:37:16 -0700

    swswsww

--- Assets/scripts/enemyHealth.cs
+++ Assets/scripts/enemyHealth.cs
@@ -5,8 +5,8 @@ using UnityEngine.UI;
 
 public class enemyHealth : MonoBehaviour
 {
-    
-    public int health = 5;
+  
+    public static int  health = 5;
     //https://www.youtube.com/watch?v=XKoSfm4DTFc
 
     // Start is called before the first frame update
@@ -18,14 +18,15 @@ public class enemyHealth : MonoBehaviour
     // Update is called once per frame
     void OnCollisionEnter2D(Collision2D col)
     {
-        //Add an && keydown x to see if that would work
-        if(col.gameObject.tag == "Player" && Input.GetMouseButton(0))
+        
+        if(col.gameObject.tag == "Player") //&& Input.GetMouseButton(0))
         {
 
             health = health-1;
             Debug.Log("Health has reduced to: " + health);
             
         }
+
         if (health <= 0)
         {
             Destroy(gameObject);
---
+++ Assets/scripts/healthBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthBar : MonoBehaviour
+{
+    public static int health;
+    public SpriteRenderer rend;
+    public Sprite health5, health4, health3, health2, health1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        health5 = Resources.Load<Sprite>("Health bar_0");
+        health4 = Resources.Load<Sprite>("Health bar test");
+        health3 = Resources.Load<Sprite>("Health bar_2");
+        health2 = Resources.Load<Sprite>("Health bar_3");
+        health1 = Resources.Load<Sprite>("Health bar_4");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health = enemyHealth.health;
+        if(health == 4)
+        {
+            rend.sprite = health4;
+        }
+    }
+}
