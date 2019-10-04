12fa7d971f68e32c07702af221ff645ea79bbef8
Glimglob <54424684+Glimglob@users.noreply.github.com>
2019-10-04 11:29:02 -0700

    working health and mana bar

--- Assets/scripts/PlayerHealthbar.cs
+++ Assets/scripts/PlayerHealthbar.cs
@@ -4,16 +4,54 @@ using UnityEngine;
 
 public class PlayerHealthbar : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int playerHealth = 100;
+    public int mana = 100;
+    public int keyCounter;
     void Start()
     {
-        Transform hBar = transform.Find("hBar");
-        hBar.localScale = new Vector3(250f, 35f);
+        
     }
-
+    
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        //Clamps
+        playerHealth = Mathf.Clamp(playerHealth, 0, 100);
+        mana = Mathf.Clamp(mana, 0, 100);
+        //Regular skill
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            keyCounter++;
+            manadeplete();
+        }
+        // Special skills
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            if(mana >= 20)
+            {
+                keyCounter++;
+                if(keyCounter == 1)
+                {
+                    mana = mana - 20;
+                    keyCounter--;
+                }
+            }
+        }
+        // bar transform
+        Transform hBar = transform.Find("hBar");
+        hBar.localScale = new Vector3(playerHealth * 4, 35f);
+        Transform mBar = transform.Find("mBar");
+        mBar.localScale = new Vector3(mana * 4, 35f);
+        
+    }
+    //Regular mana deplete
+    void manadeplete()
+    {
+        if (keyCounter == 1)
+        {
+            mana = mana - 10;
+            keyCounter--;
+        }
         
     }
 }
