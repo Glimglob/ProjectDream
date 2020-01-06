21adca0063e045fb68bf75047e03e486585c1fc6
Glimglob <54424684+Glimglob@users.noreply.github.com>
2020-01-06 13:38:58 -0800

    swswssw

---
+++ Assets/scripts/enemy/waypointSystem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class waypointSystem : MonoBehaviour
+{
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+
+    public Transform[] waypoints;
+    private int randomSpot;
+
+    private void Start()
+    {
+        waitTime = startWaitTime;
+        randomSpot = Random.Range(0, waypoints.Length);
+    }
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[randomSpot].position, speed * Time.deltaTime);
+        if(Vector2.Distance(transform.position, waypoints[randomSpot].position)< 0.2f)
+        {
+            if(waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, waypoints.Length);
+                waitTime = startWaitTime;
+
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;            }
+        }
+    }
+}
