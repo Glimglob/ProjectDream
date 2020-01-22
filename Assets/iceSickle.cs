abb24b84d4bec5879d7054f41c3d4b218e30551f
Glimglob <54424684+Glimglob@users.noreply.github.com>
2020-01-22 13:36:10 -0800

    hyfhhvjvj

---
+++ Assets/iceSickle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class iceSickle : MonoBehaviour
+{
+    public BoxCollider2D bc2d;
+    public Animator anim;
+    public float time = 2;
+    public float timeregen = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if(time <= 0)
+        {
+            
+                anim.SetBool("ready", true);
+                timeregen += Time.deltaTime;
+                if (timeregen >= 2)
+                {
+                    time = 2;
+                    anim.SetBool("ready", false);
+                }
+            
+        }
+        if(time >= 0)
+        {
+            timeregen = 0;
+        }
+    }
+   
+}
