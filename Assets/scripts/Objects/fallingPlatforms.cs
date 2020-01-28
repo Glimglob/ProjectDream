7583daa6ed61f7fabb88ce7338f028e37e8f52bb
Glimglob <54424684+Glimglob@users.noreply.github.com>
2020-01-28 13:34:15 -0800

    kmflmsklmrlkggreggrerge

---
+++ Assets/scripts/Objects/fallingPlatforms.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallingPlatforms : MonoBehaviour
+{
+    public float fallDelay = 2.0f;
+    public Rigidbody2D rb2d;
+    public float speed;
+    public float time = 0;
+    private void Update()
+    {
+        if (time >= 2)
+        {
+            rb2d.velocity = transform.up * speed;
+            time = 0;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "player")
+        {
+            rb2d.velocity = transform.up * -speed;
+            timecount();
+        }
+    }
+    void timecount()
+    {
+        time += Time.deltaTime;
+        
+    }
+
+    
+}
