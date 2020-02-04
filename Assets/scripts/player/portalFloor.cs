87abbdc827e65e207a28d9bbc3d3348162eb7db1
Glimglob <54424684+Glimglob@users.noreply.github.com>
2020-02-04 13:42:32 -0800

    uhghbgbb

---
+++ Assets/scripts/player/portalFloor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class portalFloor : MonoBehaviour
+{
+    public Transform destination;
+    public Transform player;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            rb = collision.GetComponent<Rigidbody2D>();
+            player.transform.position = destination.position;
+            rb.Sleep();
+        }
+
+    }
+}
