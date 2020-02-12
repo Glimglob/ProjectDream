a3bb41cece81bbaa4a5ba8c0f8fee901c780c1f2
Glimglob <54424684+Glimglob@users.noreply.github.com>
2020-02-12 13:32:47 -0800

    ffsda

---
+++ Assets/treebosstwo.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class treebosstwo : MonoBehaviour
+{
+    public Transform leftAcorn;
+    public Transform rightAcorn;
+    public GameObject Acorn;
+    public float acornCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        InvokeRepeating("acornleft", 5, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(acornCount == 5)
+        {
+            CancelInvoke();
+            bossSecond();
+        }
+        if(acornCount >= -5)
+        {
+            CancelInvoke();
+            bossStart();
+        }
+    }
+    void bossStart()
+    {
+        
+    }
+    void bossSecond()
+    {
+        InvokeRepeating("acornRight", 5, 1);
+    }
+    void acornleft()
+    {
+      Instantiate(Acorn, leftAcorn.position, Quaternion.identity);
+        acornCount += 1;
+    }
+    void acornRight()
+    {
+        Instantiate(Acorn, rightAcorn.position, Quaternion.identity);
+        acornCount -= 1;
+    }
+}
