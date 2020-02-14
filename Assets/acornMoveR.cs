af1f3359047eeb09ef795e73320af31624dfcf9c
Glimglob <54424684+Glimglob@users.noreply.github.com>
2020-02-14 13:37:48 -0800

    fdadfaffddfsdsf

---
+++ Assets/acornMoveR.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class acornMoveR : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public Rigidbody2D rigidbody2d;
+    public CircleCollider2D rc2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rc2d = transform.GetComponent<CircleCollider2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rigidbody2d.velocity = new Vector2(speed, rigidbody2d.velocity.y);
+    }
+}
--- Assets/treebosstwo.cs
+++ Assets/treebosstwo.cs
@@ -7,37 +7,56 @@ public class treebosstwo : MonoBehaviour
     public Transform leftAcorn;
     public Transform rightAcorn;
     public GameObject Acorn;
+    public GameObject AcornR;
     public float acornCount;
+    public Animator anim;
+    public float timer;
     // Start is called before the first frame update
     void Start()
     {
-
+        
         InvokeRepeating("acornleft", 5, 1);
+        anim = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
-    
-    
-    
-    void acornleft()
+    private void Update()
     {
-      Instantiate(Acorn, leftAcorn.position, Quaternion.identity);
+        
+    }
+
+
+    public void acornleft()
+    {
+        
+        
+        Instantiate(Acorn, leftAcorn.position, Quaternion.identity);
+        
         acornCount += 1;
         if(acornCount == 5)
         {
             CancelInvoke();
+            anim.SetBool("acorn Left", false);
+            anim.SetBool("look left", false);
+            anim.SetBool("look right", true);
             InvokeRepeating("acornright", 5, 1);
         }
     }
-    void acornright()
+   public void acornright()
     {
-        Instantiate(Acorn, rightAcorn.position, Quaternion.identity);
+        anim.SetBool("acorn Right", true);
+        Instantiate(AcornR, rightAcorn.position, Quaternion.identity);
         acornCount -= 1;
         if(acornCount == 0)
         {
             CancelInvoke();
+            anim.SetBool("acorn Right", false);
+            anim.SetBool("look right", false);
+            anim.SetBool("look left", true);
             InvokeRepeating("acornleft", 5, 1);
         }
+
     }
     
 }
