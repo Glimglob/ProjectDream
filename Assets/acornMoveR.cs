9a211f6463762342475d913eb5f75a380b6fb7fb
Glimglob <54424684+Glimglob@users.noreply.github.com>
2020-02-20 13:33:45 -0800

    edfs

--- Assets/acornMoveR.cs
+++ Assets/acornMoveR.cs
@@ -7,25 +7,35 @@ public class acornMoveR : MonoBehaviour
     public float speed = 10.0f;
     public Rigidbody2D rigidbody2d;
     public CircleCollider2D rc2d;
+    public int touch;
+    public Animator anim;
+    public Collision collision;
     // Start is called before the first frame update
     void Start()
     {
         rc2d = transform.GetComponent<CircleCollider2D>();
-
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
+    }
+    void explode()
+    {
+        GetComponent<playerCombat>().takeDamage(20);
+        Destroy(gameObject);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        rigidbody2d.velocity = new Vector2(speed, rigidbody2d.velocity.y);
+       
+            rigidbody2d.velocity = new Vector2(speed, rigidbody2d.velocity.y);
+        
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        collision.GetComponent<playerCombat>().takeDamage(20);
-        Destroy(gameObject);
+        anim.SetBool("Explode", true);
+        explode();
     }
 }
